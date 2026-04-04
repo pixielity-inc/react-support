7ebeb20da278aee9de6477c080e31d60802cd3c1
Pixility, Inc. <pixielity@gmail.com>
2026-04-05 00:24:24 +0200

    ♻️ refactor(registry): remove RegistryCollection adapter, use MapCollection directly in BaseRegistry

--- src/registry/index.ts
+++ src/registry/index.ts
@@ -1,11 +1,8 @@
 /**
  * @fileoverview Registry exports
  *
- * This file exports all registry-related classes and interfaces.
- *
  * @module @abdokouta/support
  * @category Registries
  */
 
 export { BaseRegistry } from "./base-registry";
-export type { BaseRegistryOptions, ValidationResult } from "./base-registry";
